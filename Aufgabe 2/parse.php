968a13ecd498262f9e74f60fce9a1a5fc71e5f1c
Sa-phi-ra <Saphira@edr22655.dip.tu-dresden.de>
2017-11-16 16:15:39 +0100

    Kommentare, hoffe passt so

--- Aufgabe 2/parse.php
+++ Aufgabe 2/parse.php
@@ -1,12 +1,14 @@
 <?php
  
-
+// fordert die php datei an
 require 'world_data_parser.php';
 
+//holt die CSV Datei aus dem Data Ordner 
 $parser = new WorldDataParser();
 $data = $parser->parseCSV('data/world_data_v1.csv');
 
 echo '<pre>';
+// schreibt die Datei aus dem Dataordner, r - nur lesen 
 print_r($data);
 echo '</pre>';
 
