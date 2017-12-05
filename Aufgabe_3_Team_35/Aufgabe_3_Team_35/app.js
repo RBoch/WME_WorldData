eedc623acfc154298690920281a72a6de02c0cfd
RBoch <bochmann.robert@outlook.com>
2017-12-05 16:55:52 +0100

    Get calls für items, items/id und items/id1/id2

--- Aufgabe_3_Team_35/Aufgabe_3_Team_35/app.js
+++ Aufgabe_3_Team_35/Aufgabe_3_Team_35/app.js
@@ -23,8 +23,8 @@ app.use( express.static( path.join(__dirname, "public") ) );
 // Globale Variable für World Data JSON
 global.jsonWorld;
 
-// Einbinden des File System Modules zum schreiben der JSON Datei
-const fs = require("fs");
+/*// Einbinden des File System Modules zum schreiben der JSON Datei
+const fs = require("fs");*/
 
 // Pfad zur World_Data.csv
 const csvPath = "world_data.csv";
@@ -40,20 +40,63 @@ converter
             // Speichern in globale Variable
             jsonWorld = jsonObj;
             // Speichern als Datei
-            var content = JSON.stringify(jsonWorld);
+           /* var content = JSON.stringify(jsonWorld);
             fs.writeFile("world_data.json", content, "utf8", (err) => {
                 if(err){
                     console.log(err);
                 } else {
                     console.log("Successfuly saved file.")
                 }
-            } )
+            } )*/
         }
     } );
 
 /**************************************************************************
 ********************** handle HTTP METHODS ***********************
 **************************************************************************/
+
+function getWorldData(){
+    return jsonWorld;
+}
+
+function getCountryData(id){
+    for( i = 0; i < jsonWorld.length; i++){
+        if(jsonWorld[i].id == id){
+            return jsonWorld[i];
+        }
+    }
+    return "No such id {" + id + "} in database.";
+}
+
+function getRangeData(id1, id2){
+    var range = [];
+    for( i = 0; i < jsonWorld.length; i++){
+        if(jsonWorld[i].id >= id1 && jsonWorld[i].id <= id2){
+            range.push(jsonWorld[i]);
+        }
+    }
+    return range;
+}
+
+// Get /items(/id1(/id2))
+app.get("/items/:id1?/:id2?", (req, res) => {
+    var response;
+    if(req.params.id2 == null){
+        if(req.params.id1 == null){
+            // beziehe alle Daten
+            response = getWorldData();
+        } else {
+            // beziehe Daten für Land mit ID 
+            response = getCountryData(req.params.id1);
+        }
+    } else {
+        // beziehe Daten für Länder zwichen ID 1 und ID 2
+        response = getRangeData(req.params.id1,req.params.id2);
+    }
+    // sende Antwort
+    console.log(typeof response);
+    res.send(response);
+})
 
 
 // DO NOT CHANGE!
